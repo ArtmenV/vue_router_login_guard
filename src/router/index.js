720034d9fe92165795c05b9a6611dba5a8daeb0f
Artmen_V <viliutich.artem@gmail.com>
2019-10-27 18:58:59 +0700

    tttps

--- src/router/index.js
+++ src/router/index.js
@@ -42,19 +42,19 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   if (to.matched.some(record => record.meta.requiresAuth)) {
-    // этот путь требует авторизации, проверяем залогинен ли
-    // пользователь, и если нет, перенаправляем на страницу логина
     if (!store.getters.loggedIn) {
       next({
         name: "login"
       });
-    } else {
+    }
+  } else if (to.matched.some(record => record.meta.requiresVisitor)) {
+    if (store.getters.loggedIn) {
       next({
         name: "home"
       });
+    } else {
+      next();
     }
-  } else {
-    next();
   }
 });
 
